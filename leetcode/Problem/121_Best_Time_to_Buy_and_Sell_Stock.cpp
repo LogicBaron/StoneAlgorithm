82912958541080b3b20f8d8d784f21381b1cc46e
sstone <jhjh9701@gmail.com>
2023-04-25 14:31:47 +0900

    easy

--- leetcode/Problem/121_Best_Time_to_Buy_and_Sell_Stock.cpp
+++ leetcode/Problem/121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -5,7 +5,7 @@ public:
         int profit = 0;
 
         for (int i=0; i<prices.size(); i++) {
-            profit = max(profit, prices[i]-min_ever);
+            profit = max(profit, prices[i] - min_ever);
             min_ever = min(min_ever, prices[i]);
         }
 

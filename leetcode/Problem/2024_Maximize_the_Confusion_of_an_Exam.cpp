f2fbc513fb3d08f1bde07fed641af92f090f7906
sstone <jhjh9701@gmail.com>
2023-07-07 14:43:16 +0900

    medium

---
+++ leetcode/Problem/2024_Maximize_the_Confusion_of_an_Exam.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        int n = answerKey.size(), ans = k;
+        unordered_map<char, int> count;
+
+        for (int i=0; i<k; i++) {
+            count[answerKey[i]]++;
+        }
+
+        int left = 0;
+        for (int right=k; right<n; right++) {
+            count[answerKey[right]]++;
+
+            while (min(count['T'],count['F']) > k) {
+                count[answerKey[left]]--;
+                left++;
+            }
+            
+            ans = max(ans, right-left+1);
+        }
+
+        return ans;
+    }
+};

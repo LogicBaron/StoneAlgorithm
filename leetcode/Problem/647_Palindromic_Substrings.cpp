7cae6cef58028ba48370e62c6b39f180ee107673
MagnaLapis <jhjh9701@gmail.com>
2023-12-14 13:57:37 +0000

    medium

--- leetcode/Problem/647_Palindromic_Substrings.cpp
+++ leetcode/Problem/647_Palindromic_Substrings.cpp
@@ -11,26 +11,24 @@ public:
             for (int i=0; i<n-step; i++) {
                 
                 // if i == 0, all palindrome
-                if (step == 0) dp[i][i+step] = 1;
+                if (step == 0) {
+                    dp[i][i+step] = 1;
+                    ans += 1;
+                }
 
                 else if (step == 1) {
                     dp[i][i+step] = s[i]==s[i+step];
+                    ans += s[i] == s[i+step];
                 }
                 
                 else {
                     if (dp[i+1][i+step-1] && s[i]==s[i+step]) {
                         dp[i][i+step] = 1;
+                        ans += 1;
                     }
                 }
             }
         }
-
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<n; j++) {
-                ans += dp[i][j];
-            }
-        } 
-
 
         return ans;
     }
